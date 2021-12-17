1d21fc58ca39b3ba14e1817e79bdc0beca89bfc4
iglesiasana <iglesiasanavzla@gmail.com>
2021-12-17 13:21:35 -0400

    mod. dic 17 2021

--- js_index.js
+++ js_index.js
@@ -1,6 +1,18 @@
+const iconoMenu = document.getElementsByClassName('contenedor_icono_menu')[0]
+const contenedorLinksCabecera = document.getElementsByClassName('nav')[0]
 const toggleButton = document.getElementsByClassName('boton_menu')[0]
 const bandejaRecursos = document.getElementsByClassName('bandeja_recursos')[0]
+const botonSerie = document.getElementsByClassName('serie')[0]
+const bandejaIdealYrelevante = document.getElementsByClassName('bandeja_ideal_y_relevante')[0]
+
+iconoMenu.addEventListener('click', () => {
+    contenedorLinksCabecera.classList.toggle('active')
+})
 
 toggleButton.addEventListener('click', () => {
     bandejaRecursos.classList.toggle('active')
+})
+
+botonSerie.addEventListener('click', () => {
+    bandejaIdealYrelevante.classList.toggle('active')
 })
