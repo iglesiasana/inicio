74152cce3cdba45fb4e49418dcbe6fdc3528c6ff
iglesiasana <iglesiasanavzla@gmail.com>
2021-12-25 11:09:16 -0400

    mod. dic 255 2021

--- js_index.js
+++ js_index.js
@@ -4,6 +4,7 @@ const toggleButton = document.getElementsByClassName('boton_menu')[0]
 const bandejaRecursos = document.getElementsByClassName('bandeja_recursos')[0]
 const botonSerie = document.getElementsByClassName('serie')[0]
 const bandejaIdealYrelevante = document.getElementsByClassName('bandeja_ideal_y_relevante')[0]
+const cuerpoPagina = document.body
 
 iconoMenu.addEventListener('click', () => {
     contenedorLinksCabecera.classList.toggle('active')
